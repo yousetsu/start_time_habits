8b4d2830a5ab41c7b7cc10ae50e8b7b54774e9e6
yousetsu <sarujincanon@gmail.com>
2022-12-04 10:10:25 +0900

    設定画面（仮）完了、sqlテーブル作成、sql準備完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/services.dart';
 import './habits.dart';
 import './achievement.dart';
 import './setting.dart';
+import 'dart:io';
+
+const String strCnsSqlCreateRireki ="CREATE TABLE IF NOT EXISTS rireki(id INTEGER PRIMARY KEY, goaltime TEXT, realtime TEXT, status TEXT, kaku1 INTEGER, kaku2 INTEGER, kaku3 TEXT, kaku4 TEXT)";
 void main() {
+  //SQLfliteで必要？
+  WidgetsFlutterBinding.ensureInitialized();
+  //初回DB登録
+  firstRun();
+  //画面表示
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,6 +43,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
+  bool habitsFlg = false;
+  MaterialColor primaryColor = Colors.orange;
+  String strStarstop = 'START';
   @override
   void initState() {
     super.initState();
@@ -40,12 +53,100 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('第一画面')),
+      appBar: AppBar(title: const Text('勉強時間アラーム')),
       body: SingleChildScrollView(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Text('第一画面です',style:TextStyle(fontSize: 20.0)),
+              children:  <Widget>[
+                Container(
+                    margin: const EdgeInsets.all(20.0),
+                    padding: const EdgeInsets.all(50.0),
+                    alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(border: Border.all(color: Colors.lightBlueAccent), borderRadius: BorderRadius.circular(10), color: Colors.lightBlueAccent,),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:  const <Widget>[
+                        Text('習慣開始まで',style:TextStyle(fontSize: 20.0)),
+                        Text('あと　時間　分　秒',style:TextStyle(fontSize: 20.0))
+                      ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  <Widget>[
+                      Text('習慣実行回数',style:TextStyle(fontSize: 20.0)),
+                      Text('回',style:TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  <Widget>[
+                      Text('習慣連続実行回数',style:TextStyle(fontSize: 20.0)),
+                      Text('回',style:TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  <Widget>[
+                      Text('開始時間前に始めた回数',style:TextStyle(fontSize: 20.0)),
+                      Text('回',style:TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  <Widget>[
+                      Text('開始時間前に始めた連続回数',style:TextStyle(fontSize: 20.0)),
+                      Text('回',style:TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 200, height: 70,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: primaryColor, shape: const StadiumBorder(), elevation: 16,),
+                    onPressed: buttonPressed,
+                    child: Text( '習慣開始', style: const TextStyle(fontSize: 35.0, color: Colors.white,),),
+                  ),
+                ),
               ]
           )
       ),
@@ -69,4 +170,82 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+  Future<void> buttonPressed() async {
+    // habitsFlg = !habitsFlg;
+    // setState(() {
+    //   primaryColor = habitsFlg ?  Colors.blue:Colors.orange;
+    //   strStarstop = habitsFlg ?  'STOP':'START';
+    // });
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title:  Text('確認'),
+            content: Text('習慣を開始しますか？'),
+            actions: <Widget>[
+              TextButton(
+                  child: Text('はい'),
+                  onPressed: () => Navigator.pop<String>(context, 'Yes')),
+              TextButton(
+                  child: Text('ちょっと待って'),
+                  onPressed: () => Navigator.pop<String>(context, 'No')),
+            ],
+          )).then<void>((value) => resultAlert(value));
+    }
+  void resultAlert(String value) {
+    setState(() {
+      switch (value) {
+        case 'Yes':
+
+
+
+
+          //アチーブメントがあれば表示
+          // showDialog(context: context,
+          //     builder: (BuildContext context) => AlertDialog(
+          //       title: Text(AppLocalizations.of(context)!.confirm),
+          //       content: Text(AppLocalizations.of(context)!.moveforward),
+          //       actions: <Widget>[
+          //         TextButton(
+          //             child: const Text('OK'),
+          //             onPressed: () => Navigator.pop<String>(context, 'Ok')),
+          //       ],
+          //     )).then<void>((value) => resultSuccess(value));
+          break;
+        case 'No':
+          break;
+      }
+    });
+  }
+}
+
+//初回起動分の処理
+Future<void> firstRun() async {
+  String dbpath = await getDatabasesPath();
+  //設定テーブル作成
+  String path = p.join(dbpath, "internal_assets.db");
+  //設定テーブルがなければ、最初にassetsから作る
+  var exists = await databaseExists(path);
+  if (!exists) {
+    // Make sure the parent directory exists
+    //親ディレクリが存在することを確認
+    try {
+      await Directory(p.dirname(path)).create(recursive: true);
+    } catch (_) {}
+
+    // Copy from asset
+    ByteData data = await rootBundle.load(p.join("assets", "external_assets.db"));
+    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    // Write and flush the bytes written
+    await File(path).writeAsBytes(bytes, flush: true);
+
+  } else {
+    //print("Opening existing database");
+  }
+  //履歴テーブル作成
+  path = p.join(dbpath, "rireki.db");
+  await openDatabase(path, version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(strCnsSqlCreateRireki);
+      });
 }
--- lib/setting.dart
+++ lib/setting.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_picker/flutter_picker.dart';
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key); //コンストラクタ
   @override
   State<SettingScreen> createState() => _SettingScreenState();
 }
+const String strCnsEveryDay = "EveryDay";
+const String strCnsNormalDay = "NormarlDay";
+
 
 class _SettingScreenState extends State<SettingScreen> {
+  String? _type = '';
+  bool isOn = false;
+  bool isEnable = false;
+  DateTime everyTime = DateTime.utc(0, 0, 0);
+  DateTime normalTime = DateTime.utc(0, 0, 0);
+  DateTime holidayTime = DateTime.utc(0, 0, 0);
+  DateTime notificationTime = DateTime.utc(0, 0, 0);
   @override
   void initState() {
     super.initState();
@@ -14,14 +26,106 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('習慣状況')),
+      appBar: AppBar(title: const Text('設定')),
       body: SingleChildScrollView(
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Text('習慣状況',style:TextStyle(fontSize: 20.0)),
-              ]
-          )
+              mainAxisAlignment: MainAxisAlignment.start,
+          //  crossAxisAlignment: CrossAxisAlignment.start,
+              children:  <Widget>[
+                const Text('設定',style:TextStyle(fontSize: 20.0)),
+
+                Row(mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                   // const Padding(padding: EdgeInsets.only(left:50.0),),
+                    Radio(activeColor: Colors.blue, value: strCnsEveryDay, groupValue: _type, onChanged: _handleRadio, autofocus:true,),
+                    const Text('毎日', style:TextStyle(fontSize: 20.0),),
+                  ],),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.lightBlueAccent, padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 80),),
+                  onPressed: () async {
+                    Picker(
+                      adapter: DateTimePickerAdapter(type: PickerDateTimeType.kHM, value: everyTime, customColumnType: [3, 4]),
+                      title: const Text("Select Time"),
+                      onConfirm: (Picker picker, List value) {
+                        setState(() => {
+                          everyTime = DateTime.utc(2016, 5, 1, value[0], value[1], 0),
+                        //  _saveStrSetting('goalsleeptime',_goalsleeptime.toString()),
+                        });
+                      },
+                    ).showModal(context);
+                  },
+                  child: Text(style: const TextStyle(fontSize: 40),DateFormat.Hm().format(everyTime) ),
+                ),
+
+
+
+                Row(mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    // const Padding(padding: EdgeInsets.only(left:50.0),),
+                    Radio(activeColor: Colors.blue, value: strCnsNormalDay, groupValue: _type, onChanged: _handleRadio, autofocus:true,),
+                    const Text('平日／土日ごと', style:TextStyle(fontSize: 20.0),),
+
+                  ],),
+                const Text('　平日', style:TextStyle(fontSize: 20.0),),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.lightBlueAccent, padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 80),),
+                  onPressed: () async {
+                    Picker(
+                      adapter: DateTimePickerAdapter(type: PickerDateTimeType.kHM, value: normalTime, customColumnType: [3, 4]),
+                      title: const Text("Select Time"),
+                      onConfirm: (Picker picker, List value) {
+                        setState(() => {
+                          normalTime = DateTime.utc(2016, 5, 1, value[0], value[1], 0),
+                          //  _saveStrSetting('goalsleeptime',_goalsleeptime.toString()),
+                        });
+                      },
+                    ).showModal(context);
+                  },
+                  child: Text(style: const TextStyle(fontSize: 40),DateFormat.Hm().format(normalTime) ),
+                ),
+                const Text('　土日', style:TextStyle(fontSize: 20.0),),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.lightBlueAccent, padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 80),),
+                  onPressed: () async {
+                    Picker(
+                      adapter: DateTimePickerAdapter(type: PickerDateTimeType.kHM, value: holidayTime, customColumnType: [3, 4]),
+                      title: const Text("Select Time"),
+                      onConfirm: (Picker picker, List value) {
+                        setState(() => {
+                          holidayTime = DateTime.utc(2016, 5, 1, value[0], value[1], 0),
+                          //  _saveStrSetting('goalsleeptime',_goalsleeptime.toString()),
+                        });
+                      },
+                    ).showModal(context);
+                  },
+                  child: Text(style: const TextStyle(fontSize: 40),DateFormat.Hm().format(holidayTime) ),
+                ),
+
+                const Text('通知設定', style:TextStyle(fontSize: 20.0),),
+                Switch(value: isOn, onChanged: (bool? value) {
+                    if (value != null) {
+                      setState(() {isOn = value;});
+                    }
+                  },
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.lightBlueAccent, padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 80),),
+                  onPressed: () async {
+                    Picker(
+                      adapter: DateTimePickerAdapter(type: PickerDateTimeType.kHM, value: notificationTime, customColumnType: [3, 4]),
+                      title: const Text("Select Time"),
+                      onConfirm: (Picker picker, List value) {
+                        setState(() => {
+                          notificationTime = DateTime.utc(2016, 5, 1, value[0], value[1], 0),
+                          //  _saveStrSetting('goalsleeptime',_goalsleeptime.toString()),
+                        });
+                      },
+                    ).showModal(context);
+                  },
+                  child: Text(style: const TextStyle(fontSize: 40),DateFormat.Hm().format(notificationTime) ),
+                ),
+              ],
+          ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 2,
@@ -42,5 +146,19 @@ class _SettingScreenState extends State<SettingScreen> {
         },
       ),
     );
+  }
+  /*------------------------------------------------------------------
+設定画面プライベートメソッド
+ -------------------------------------------------------------------*/
+//ラジオボタン選択時の処理
+  void _handleRadio(String? e){
+    setState(() {
+      _type = e;
+      if(e == strCnsEveryDay){
+        isEnable = false;
+      }else{
+        isEnable = true;
+      }
+    });
   }
 }
